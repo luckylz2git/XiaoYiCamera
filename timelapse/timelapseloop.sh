b6f5043d796131e2553edf98b28c4bb2ac7dfeeb
luckylz2git <8618993@qq.com>
2015-07-02 22:47:40 +0800

    Update timelapseloop.sh

--- timelapse/timelapseloop.sh
+++ timelapse/timelapseloop.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
-old=`cat /tmp/fuse_d/timelapse.conf`
+if [ ! -f /tmp/fuse_d/timelapse_count ]; then
+  echo `ls -lR /tmp/fuse_d/DCIM/ | grep "RAW" | wc -l` > /tmp/fuse_d/timelapse_count
+fi
+old=`cat /tmp/fuse_d/timelapse_count`
 old=$(($old + $1))
 new=`ls -lR /tmp/fuse_d/DCIM/ | grep "RAW" | wc -l`
 if [ $new -ge $old ]; then
   sleep 1
+  rm /tmp/fuse_d/timelapse_count
   mv /tmp/fuse_d/autoexec.ash /tmp/fuse_d/autoexec-timelapse.ash
   mv /tmp/fuse_d/autoexec-poweroff.ash /tmp/fuse_d/autoexec.ash
-  sleep 9
+  sleep 5
   reboot
 fi
