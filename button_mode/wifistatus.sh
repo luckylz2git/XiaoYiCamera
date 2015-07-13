6dd278f16ab88f7bbc48cd1c68a41f8c42657177
luckylz2git <8618993@qq.com>
2015-07-13 18:45:12 +0800

    echo substring usage

--- button_mode/wifistatus.sh
+++ button_mode/wifistatus.sh
@@ -4,3 +4,10 @@ HOST="192.168.42.1"
 count=$(ping -W 1 -c 1 $HOST | grep 'received')
 echo $count > /tmp/fuse_d/ping/ping-`date '+%H%M%S'`.txt
 
+$ str='123abc'
+$ echo $str
+123abc
+$ echo ${str:0:3}
+123
+$ echo ${str:3:3}
+abc
