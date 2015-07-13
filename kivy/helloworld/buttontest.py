2bd4d100a28f469d21fb465ee08ecd7c92949f69
luckylz2git <8618993@qq.com>
2015-07-14 00:06:00 +0800

    Update buttontest.py

--- kivy/helloworld/buttontest.py
+++ kivy/helloworld/buttontest.py
@@ -4,17 +4,17 @@ from kivy.uix.widget import Widget
 from kivy.app import App
 
 class HelloWorldApp(App):
-	def build(self):
-		par = Widget()
-		btn1 = Button(text='Hello world 1')
-		btn1.bind(on_press=callback)
-		btn2 = Button(text='Hello world 2')
-		btn2.bind(on_press=callback)
-		par.add_widget(btn1)
-		par.add_widget(btn2)
-		return par
-	def callback(instance):
-		print('The button <%s> is being pressed' % instance.text)		
+  def build(self):
+    par = Widget()
+    btn1 = Button(text='Hello world 1')
+    btn1.bind(on_press=callback)
+    btn2 = Button(text='Hello world 2')
+    btn2.bind(on_press=callback)
+    par.add_widget(btn1)
+    par.add_widget(btn2)
+    return par
+  def callback(instance):
+    print('The button <%s> is being pressed' % instance.text)		
 		
 HelloWorldApp().run()
 #python buttontest.py
