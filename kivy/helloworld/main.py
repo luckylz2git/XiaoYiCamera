c3a65cbb3afe5e9de733ef8e2ac53b77d8e3d7f8
luckylz2git <8618993@qq.com>
2015-07-15 13:40:47 +0800

    add linux commands

--- kivy/helloworld/main.py
+++ kivy/helloworld/main.py
@@ -1,9 +1,14 @@
-import kivy
+import kivy, os
 kivy.require('1.0.9')
 from kivy.lang import Builder
 from kivy.uix.gridlayout import GridLayout
-from kivy.properties import NumericProperty
+from kivy.properties import NumericProperty, StringProperty
 from kivy.app import App
+
+if os.name == 'nt':
+    from kivy.core.window import Window
+    Window.size = (600, 800)
+print os.name
 
 __version__='0.0.1'
 
@@ -13,12 +18,13 @@ Builder.load_string('''
     Label:
         text: 'Welcome to the Hello world'
     Button:
-        text: 'Click me! %d' % root.counter
+        text: '{} Click me! {}'.format(root.osname, root.counter)
         on_release: root.my_callback()
 ''')
 
 class HelloWorldScreen(GridLayout):
     counter = NumericProperty(0)
+    osname = StringProperty(os.name)
     def my_callback(self):
         print 'The button has been pushed'
         self.counter += 1
