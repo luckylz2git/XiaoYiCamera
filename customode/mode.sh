4f4ccd7dee60679d5253013b913d036ca4dc939a
luckylz2git <8618993@qq.com>
2015-07-17 17:42:33 +0800

    Custom Mode beta 0.0.2

--- customode/mode.sh
+++ customode/mode.sh
@@ -2,18 +2,18 @@
 
 # Default XiaoYi IP Address
 HOST="192.168.42.1"
-MODE=/tmp/fuse_a/custom/mode.inf
+MODE=$1
 LOGS=/tmp/fuse_a/custom/mode.log
 
-#GPIO=`cat /proc/ambarella/gpio`
+GPIO=`cat /proc/ambarella/gpio`
 #GPIO: General Purpose Input Output
-#WIFI=${GPIO:11:1}
+WIFI=${GPIO:11:1}
 # Get WiFi Status
-WIFI=$(ping -W 1 -c 1 $HOST | grep "received")
+#WIFI=$(ping -W 1 -c 1 $HOST | grep "received")
 # First time WiFi ON, record all JPG files count to LOGS
 # First time WiFi OFF, LOGS exist check all JPG files again
-# if [ ${WIFI} -eq 1 ]; then
-if [ -n "${WIFI}" ]; then
+# if [ -n "${WIFI}" ]; then
+if [ ${WIFI} -eq 1 ]; then
   if [ ! -f $LOGS ]; then
     echo `ls -lR /tmp/fuse_d/DCIM/ | grep "YDXJ" | wc -l` > $LOGS
   fi
@@ -28,12 +28,9 @@ else
       if [ $JPG -ge 3 ]; then
         MP4=`echo "${TOP}" | grep "YDXJ" | head -3 | grep ".mp4" | wc -l`
         if [ $MP4 -eq 0 ]; then
-          STATUS=`cat $MODE`
-          if [ "${STATUS}" == "Normal" ]; then
-            echo "Advanced" > $MODE
+          if [ "${MODE}" == "normal" ]; then
             cp -f /tmp/fuse_a/custom/advanced.ash /tmp/fuse_d/autoexec.ash
           else
-            echo "Normal" > $MODE
             cp -f /tmp/fuse_a/custom/normal.ash /tmp/fuse_d/autoexec.ash
           fi
           sleep 1
--- customode/normal.ash
+++ customode/normal.ash
@@ -1,10 +1,9 @@
 #normal mode
 sleep 3
 lu_util exec 'rm /tmp/fuse_a/custom/mode.log'
-lu_util exec 'echo "Normal" > /tmp/fuse_a/custom/mode.inf'
 sleep 1
 while true
 do
   sleep 9
-  lu_util exec '/tmp/fuse_a/custom/mode.sh'
+  lu_util exec '/tmp/fuse_a/custom/mode.sh normal'
 done
