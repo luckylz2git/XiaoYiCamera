b37ef1c708c1d497daedcd8c7f2a54e09b31a92f
luckylz2git <8618993@qq.com>
2015-07-17 17:47:08 +0800

    WiFi Mode beta 0.0.1

---
+++ wifimode/wifi.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# Default XiaoYi IP Address
+HOST="192.168.42.1"
+MODE=$1
+LOGS=/tmp/fuse_a/custom/mode.log
+
+GPIO=`cat /proc/ambarella/gpio`
+#GPIO: General Purpose Input Output
+WIFI=${GPIO:11:1}
+# Get WiFi Status
+#WIFI=$(ping -W 1 -c 1 $HOST | grep "received")
+# First time WiFi ON, record all JPG files count to LOGS
+# First time WiFi OFF, LOGS exist check all JPG files again
+# if [ -n "${WIFI}" ]; then
+if [ ${WIFI} -eq 1 ]; then
+  if [ ! -f $LOGS ]; then
+    echo `ls -lR /tmp/fuse_d/DCIM/ | grep "YDXJ" | wc -l` > $LOGS
+  fi
+else
+  if [ -f $LOGS ]; then
+    OLD=`cat $LOGS`
+    NEW=`ls -lR /tmp/fuse_d/DCIM/ | grep "YDXJ" | wc -l`
+    rm $LOGS
+    if [ $NEW -ge $(($OLD + 3)) ]; then
+      TOP=`ls -lcR /tmp/fuse_d/DCIM/ | grep "YDXJ" | head -6`
+      JPG=`echo "${TOP}" | grep ".jpg" | wc -l`
+      if [ $JPG -ge 3 ]; then
+        MP4=`echo "${TOP}" | grep "YDXJ" | head -3 | grep ".mp4" | wc -l`
+        if [ $MP4 -eq 0 ]; then
+          if [ "${MODE}" == "ap" ]; then
+            cp -f /tmp/fuse_a/custom/stamode.ash /tmp/fuse_d/autoexec.ash
+          else
+            cp -f /tmp/fuse_a/custom/apmode.ash /tmp/fuse_d/autoexec.ash
+          fi
+          sleep 1
+          reboot
+        fi
+      fi
+    fi
+  fi
+fi
