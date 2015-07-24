8e4a6db42f03c6c5a20d7c8d59f1c01ffcebf338
luckylz2git <8618993@qq.com>
2015-07-24 23:10:03 +0800

    test more msg_id

--- Ponerine/test.py
+++ Ponerine/test.py
@@ -1,8 +1,23 @@
 from camera import Camera
 
-new=Camera()
+new=Camera("192.168.1.123")
 print new
 new.LinkCamera()
+new.SendMsg('{"msg_id":2,"type":"buzzer_ring", "param":"on"}')
+new.SendMsg('{"msg_id":2,"type":"capture_mode", "param":"precise quality"}')
+new.SendMsg('{"msg_id":2,"type":"preview_status", "param":"off"}')
+new.SendMsg('{"msg_id":2,"type":"buzzer_ring", "param":"off"}')
+#show all configs
 new.SendMsg('{"msg_id":3}')
-new.SendMsg('{"msg_id":9,"param":"buzzer_ring"}')
-
+#new.SendMsg('{"msg_id":3,"param":"dual_stream_status"}')
+#new.SendMsg('{"msg_id":9,"param":"buzzer_ring"}')
+new.SendMsg('{"msg_id":3,"param":"capture_mode"}')
+new.SendMsg('{"msg_id":9,"param":"capture_mode"}')
+'''
+video_resolution
+rec_mode record_photo
+'''
+#new.SendMsg('{"msg_id":513}')
+#new.SendMsg('{"msg_id":514}')
+#new.SendMsg('{"msg_id":515}')
+#new.SendMsg('{"msg_id":2,"type":"rec_mode","param":"record_photo"}')
