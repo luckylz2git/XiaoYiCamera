1cda024a2a4390e9ded1ba39efc637c91865a591
luckylz2git <8618993@qq.com>
2015-07-25 12:23:50 +0800

    Update test.py

--- Ponerine/test.py
+++ Ponerine/test.py
@@ -4,15 +4,20 @@ new=Camera("192.168.1.123")
 print new
 new.LinkCamera()
 new.SendMsg('{"msg_id":2,"type":"buzzer_ring", "param":"on"}')
+new.SendMsg('{"msg_id":2,"type":"buzzer_ring", "param":"off"}')
 new.SendMsg('{"msg_id":2,"type":"capture_mode", "param":"precise quality"}')
 new.SendMsg('{"msg_id":2,"type":"preview_status", "param":"off"}')
-new.SendMsg('{"msg_id":2,"type":"buzzer_ring", "param":"off"}')
 #show all configs
-new.SendMsg('{"msg_id":3}')
+#new.SendMsg('{"msg_id":3}')
 #new.SendMsg('{"msg_id":3,"param":"dual_stream_status"}')
 #new.SendMsg('{"msg_id":9,"param":"buzzer_ring"}')
 new.SendMsg('{"msg_id":3,"param":"capture_mode"}')
 new.SendMsg('{"msg_id":9,"param":"capture_mode"}')
+#SD Card Usage
+new.SendMsg('{"msg_id":5,"type":"total"}')
+new.SendMsg('{"msg_id":5,"type":"free"}')
+#Battery Status
+new.SendMsg('{"msg_id":13}')
 '''
 video_resolution
 rec_mode record_photo
@@ -21,4 +26,5 @@ rec_mode record_photo
 #new.SendMsg('{"msg_id":514}')
 #new.SendMsg('{"msg_id":515}')
 #new.SendMsg('{"msg_id":2,"type":"rec_mode","param":"record_photo"}')
-new.SendMsg('{"msg_id":258}')
+#new.SendMsg('{"msg_id":258}')
+#new.SendMsg('{"msg_id":769}')
